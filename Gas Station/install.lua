9d4ef8b9571f6afffa6f9ba9eda948424cf741ac
Artem <krutog727@gmail.com>
2022-09-10 17:49:23 +0300

    add installer

---
+++ Gas Station/install.lua
@@ -0,0 +1,31 @@
+local internet = require("Internet")
+local fs = require("Filesystem")
+local system = require("System")
+
+print("Creating a folder")
+fs.makeDirectory("/Applications/Cass.app")
+print("Succes!")
+
+print("Downloading Main.lua")
+internet.download("https://raw.githubusercontent.com/Bumer-32/Minecraft-opencomputers/main/Gas%20Station/Cass.app/Main.lua", "/Applications/Cass.app/Main.lua")
+print("Succes!")
+
+print("Downloading Config.cfg")
+internet.download("https://raw.githubusercontent.com/Bumer-32/Minecraft-opencomputers/main/Gas%20Station/Cass.app/Config.cfg", "/Applications/Cass.app/Config.cfg")
+print("Succes!")
+
+print("Downloading Icon.pic")
+internet.download("https://raw.githubusercontent.com/Bumer-32/Minecraft-opencomputers/main/Gas%20Station/Cass.app/Icon.pic", "/Applications/Cass.app/Icon.pic")
+print("Succes!")
+
+print("Downloading Logo.pic")
+internet.download("https://raw.githubusercontent.com/Bumer-32/Minecraft-opencomputers/main/Gas%20Station/Cass.app/Logo.pic", "/Applications/Cass.app/Logo.pic")
+print("Succes!")
+
+print("Creating shortcut")
+system.createShortcut(
+  "/MineOS/" .. system.getUser() .. "/Desktop/Cass",
+  "system.createShortcut(
+  "/Applications/Cass.app/"
+)
+print("Succes!")
